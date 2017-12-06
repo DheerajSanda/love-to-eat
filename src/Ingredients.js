6a2c898528a89cec7efa570ce5b351f92fc1dc48
DheerajSanda <dheeraj.sanda@gmail.com>
2017-12-06 10:34:41 -0800

    2 commit

---
+++ src/Ingredients.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+class Ingredients extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {};
+    this.addIngredients = this.addIngredients.bind(this);
+  }
+
+  addIngredients() {
+    console.log('Ingredient Added');
+    console.log(this.quantity.value, this.ingredient.value);
+    this.props.addIngredient(this.quantity.value, this.ingredient.value);
+
+    this.quantity.value = "";
+    this.ingredient.value = "";
+  }
+
+  render(){
+    return(
+      <div>
+        <div className="form-inline form-group">
+          <label htmlFor="quantity">Quantity</label>
+          <input type="text"
+            className="form-control"
+            id="quantity"
+            ref={(input) => {this.quantity = input;}}
+            placeholder="Quantity" />
+          <label htmlFor="ingrdient">Ingredient</label>
+          <input type="text"
+            className="form-control"
+            ref={(input) => {this.ingredient = input;}}
+            id="ingredient"
+            placeholder="Ingredient" />
+          <button type="button" onClick={this.addIngredients} className="btn btn-info">Add</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Ingredients;
